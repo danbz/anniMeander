f908962d6732f454ebe1917582ec6905340cab32
danb <dan@buzzo.com>
2018-12-30 18:10:01 +0000

    added weighted probability for edge colors

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -96,15 +96,27 @@ void ofApp::dragEvent(ofDragInfo dragInfo){
 }
 
 void ofApp::meander(){
-    int col =0;
+    // create a red or white meander by choosing colors in red or white to put into vector for width x height
+    // TBD add in logic to create continuous paths of red and white with context sensitivity to other blocks around each new block ?
+    // TBD make markov chain like probability for edges and bifurcations?
     colorOrder.clear();
     ofColor newColor;
     for (int i=0; i< ofGetHeight()-boxSize; i+=boxSize){
         for (int j=0; j< ofGetWidth()-boxSize; j+=boxSize) {
-            if (ofRandom(1)<0.5){
-                newColor.set(210 , 70, 14); // from anni albers 'red meander'
+            if ((i==0) or (i > ofGetHeight()-boxSize*2) or (j==0) or (j> ofGetWidth()-boxSize*2) ){
+        
+                if (ofRandom(1)>0.1){ // make red more likely at a screen border
+                    newColor.set(210 , 70, 14); // colors taken from anni albers 'red meander' textile version
+                } else {
+                    newColor.set(206 , 210, 156);
+                }
             } else {
-                newColor.set(206 , 210, 156);
+                if (ofRandom(1)<0.5){
+                    newColor.set(210 , 70, 14); // colors taken from anni albers 'red meander' textile version
+                } else {
+                    newColor.set(206 , 210, 156);
+                   // newColor.set(0 , 210, 200);
+                }
             }
             colorOrder.push_back(newColor);
             ofDrawRectangle(j, i, boxSize,  boxSize);
